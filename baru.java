b2c05671d614f4a0cb6bbd22674024d3b569bb1c
pateh27 <fathzfl@gmail.com>
2023-09-05 19:24:12 +0700

    hehehe

--- baru.java
+++ baru.java
@@ -1,5 +1,6 @@
 public class baru {
     public static void public static void main(String[] args) {
-        
+        System.out.println("loh ga bahaya ta?");
+        system.out.println("yo bahaya seh");
     }
 }
